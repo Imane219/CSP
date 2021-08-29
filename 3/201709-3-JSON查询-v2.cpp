de73621a05d0cb8bf37946cf820bb30600dd5879
Imane219 <yiman420@foxmail.com>
2021-08-30 00:03:38 +0800

    update

---
+++ 3/201709-3-JSON查询-v2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+unordered_map<string, string> ans;
+
+string read()
+{
+  char c = getchar();
+  if(c == '{')
+    return "{";
+  else if(c == '}')
+    return "}";
+  else if(c == '"')
+
+}
